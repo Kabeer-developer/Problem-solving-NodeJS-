d29aec7bd59817eeb39c657d72bbfaf9ef280966
kabeer AR <kabeeratwork7@gmail.com>
2026-03-06 17:07:25 +0530

    JWT Auth

---
+++ jwtAuth.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const jwt = require("jsonwebtoken")
+const SECRET = "abcdef";
+
+const app = express();
+
+app.use(express.json());
+//Middleware
+function auth(req,res,next){
+    const token = req.headers.authorization?.split(' ')[1];
+    if(!token) res.status(401).json({error : "No Token"});
+
+    try{
+        req.user = jwt.verify(token,SECRET);
+        next();
+    } catch(error){
+        res.status(401).json({error : "Invalid Token"});
+    }
+}
+app.get("/",(req,res)=> {
+    res.send("<h1>JWT is Working</h1>");
+});
+
+app.post("/login",(req,res)=> {
+    const user = {id:1,username:"kabeer"}
+    const token = jwt.sign(user,SECRET);
+    res.json({token});
+})
+
+app.get("/protected",auth,(req,res)=> {
+    res.json({message : "secret data", user:req.user});
+})
+
+PORT = 5000;
+
+app.listen(PORT,()=> console.log(`server is running on http://localhost:${PORT}`))
